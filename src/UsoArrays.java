a6ce7c1288759764dac2494da3ee80d490da8c0f
dleonm02 <dleonm02@educarex.es>
2023-12-05 18:18:36 +0100

    Inicio Proyecto

---
+++ src/UsoArrays.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class UsoArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			 
+			Arrays miArray=new Arrays(5); 
+			
+			 miArray.getDatosArrays(); 
+			 } 
+		 
+	}
+
